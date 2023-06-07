a3804eeab4442d8a93ac43aaa681f396a1d12e02
Thejusl999 <tj.conquerer@gmail.com>
2023-06-07 16:32:58 +0530

    Task-3 of DOM MANIPULATIONS

---
+++ TASK-3(DOM-Task-1).js
@@ -0,0 +1,8 @@
+//FOR QUESTION 3) - Make the title have a black border as the youtuber does
+let headerTitle=document.getElementById('main-header');
+headerTitle.style.borderBottom='solid 3px #000';
+
+//FOR QUESTION 3) - Now make ADD ITEM bold and change the font color to greeen
+let addItemTitle=document.getElementsByClassName('title')
+addItemTitle[0].style.fontWeight='bold';
+addItemTitle[0].style.color='green';
